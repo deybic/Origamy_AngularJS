d0b83174cefc8c9d9d02807e43bb684ce34cd17f
Kevin Blanco <kevin@schoolmint.com>
2014-10-01 15:55:37 -0600

    Adding first 2 classes to the repo

---
+++ Clase-2/Controllers/app.js
@@ -0,0 +1,51 @@
+//Angular.js app definition
+var app = angular.module('origamy',[ ]);
+
+
+//Controller definition
+app.controller('OrigamyCtrl', ['$scope', function($scope){
+    
+    //Scope function
+    $scope.double = function(value) { 
+        return value * 2; 
+    };
+    
+
+    //Schools for the repeater 
+    $scope.schools = [
+        {
+            "name"      :"San Antonio School", 
+            "email"     :"admin@sananthonio.net",
+            "contact"   :"Rosemary"
+        },             
+        {
+                           
+            "name"      :"Tech Central School", 
+            "email"     :"admin@techschool.net",
+            "contact"   :"Any"
+        },             
+        {
+            "name"      :"South West School", 
+            "email"     :"admin@southwschool.net",
+            "contact"   :"Gabriel"
+        }
+    ];
+    
+    
+    //Add a new school
+    $scope.addSchool = function(){
+        
+        $scope.schools.push({
+            name    : $scope.new_name,
+            email   : $scope.new_email,
+            contact : $scope.new_contact
+        });
+        
+        $scope.new_name     = "";
+        $scope.new_email    = "";
+        $scope.new_contact  = "";
+        
+    }
+    
+    
+}]);
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Origamy_AngularJS
+Ejemplos Angularjs - Origamy Academy
 =================
 
-Este repositorio contiene los ejemplos mostrados durante el curso de Origamy Academy de Angular.js. 
+Este repositorio contiene los ejemplos mostrados durante el curso de Origamy Academy de Angular.js dividido por carpetas.
+
+Las presentaciones se encuentran en Slidebean en el siguiente enlace: [https://slidebean.com/kevinblanco](https://slidebean.com/kevinblanco)
+
+
