16e1778e7377482fd4a9a0edbada19e1f950fe22
Kevin Blanco <kevin@schoolmint.com>
2014-10-17 15:33:00 -0600

    Clase 4

---
+++ Clase-4/Animaciones/Enter:Leave/js/index.js
@@ -0,0 +1,22 @@
+
+var origamiAnimaciones = angular.module('origamiAnimate', ['ngAnimate']);
+
+origamiAnimaciones.controller('myCtrl', [ '$scope', function($scope){
+
+	$scope.songs = [
+	    'Sgt. Peppers Lonely Hearts Club Band',
+	    'With a Little Help from My Friends',
+	    'Lucy in the Sky with Diamonds',
+	    'Getting Better' ,
+	    'Fixing a Hole',
+	    'Shes Leaving Home',
+	    'Being for the Benefit of Mr. Kite!' ,
+	    'Within You Without You',
+	    'When Im Sixty-Four',
+	    'Lovely Rita',
+	    'Good Morning Good Morning',
+	    'Sgt. Peppers Lonely Hearts Club Band (Reprise)','A Day in the Life'
+    ];
+
+}]);
+
---
+++ Clase-4/Bower & Grunt/src/scripts/app.js
@@ -0,0 +1,8 @@
+/**
+ * @module     helloWorld
+ * @desc       Main Project Angular Module.
+ * @author     Kevin Blanco <me@kevin-blanco.com>
+ * @repo       git@github.com:kevinblanco/angular-skeleton.git
+ */
+ 
+angular.module('helloWorld', ['ui.bootstrap']);
